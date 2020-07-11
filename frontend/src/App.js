f5b7be253731c445b27f0e6cf7698f7f14c739be
JacksonPassos <jacksonguitar33@gmail.com>
2020-07-11 19:48:22 -0300

    importando css e imagens dentro do projeto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import './App.css'
+import backgroundImage from './assets/background.jpg'
 import Header from './components/Header'
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <img width={300} src={backgroundImage}></img>
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
       </ul>
