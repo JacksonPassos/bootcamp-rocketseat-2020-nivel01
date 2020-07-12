da61c22da25c7710cd76978aaacd1f0696961f0f
JacksonPassos <jacksonguitar33@gmail.com>
2020-07-11 22:42:09 -0300

    listando e cadastrando novos projetos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
+
 import './App.css'
-import backgroundImage from './assets/background.jpg'
 import Header from './components/Header'
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvendo um app', 'Frontend Web'])
+  const [projects, setProjects] = useState([])
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto: ${Date.now()}`])
-    console.log(projects)
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
+  async function handleAddProject() {
+    //setProjects([...projects, `Novo projeto: ${Date.now()}`])
+    const response = await api.post('projects', {
+      title: `Novo projeto: ${Date.now()}`,
+      owner: "Jackson Passos"
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
+
   }
 
   return (
     <>
       <Header title="Projects" />
-
-      <img width={300} src={backgroundImage}></img>
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
